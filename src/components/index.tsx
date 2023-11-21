b715b639548503d605274aefb74447317c6e750c
scarroza2bext <sebastian.carroza.ext@redsalud.cl>
2023-11-21 16:42:07 -0300

    Add practice 2

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,59 +1,26 @@
-import { Box, Button, Checkbox, Stack } from '@mui/material'
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
+import PracticeOne from './practice-1'
+import PracticeTwo from './practice-2'
+import { Card, Stack, Typography } from '@mui/material'
 
 const Home: FC = () => {
-  const [checked, setChecked] = useState(false)
-  const [error, setError] = useState(false)
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked)
-  }
-
-  const toggleError = () => {
-    setError(!error)
-  }
-
-  const getStatus = () => {
-    if (error) return 'error.main'
-    if (!checked) return 'primary.main'
-    if (checked) return 'success.main'
-  }
-
-  const getStatusName = () => {
-    if (error) return '-error'
-    if (!checked) return ''
-    if (checked) return '-checked'
-  }
-  const label = { inputProps: { 'aria-label': 'Checkbox demo' } }
   return (
     <>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Checkbox {...label} checked={checked} onChange={handleChange} />
-        {getStatus()}
-        <Box
-          className={`--component-border${getStatusName()} --corner-s-1 --component-surface${getStatusName()}`}
-          sx={{
-            width: '150px',
-            height: '150px'
-            // bgcolor: `${getStatus()}`
-          }}
-        />
-      </Stack>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-        mt={2}
-      >
-        <Button variant="contained" onClick={() => toggleError()}>
-          On / Off Error
-        </Button>
-      </Stack>
+      <Card sx={{ m: 3, p: 3 }}>
+        <Typography variant="h4" sx={{ my: 2 }}>
+          Ejercicio 1
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <PracticeOne type={'1'} />
+          <PracticeOne type={'2'} />
+        </Stack>
+      </Card>
+      <Card sx={{ m: 3, p: 3 }}>
+        <Typography variant="h4" sx={{ my: 2 }}>
+          Ejercicio 2
+        </Typography>
+        <PracticeTwo />
+      </Card>
     </>
   )
 }
